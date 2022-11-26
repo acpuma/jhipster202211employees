0342ebe2d70871bbfcbac9b25d0df74194166388
Cuma Cakmak <cumacakmak@gmail.com>
2022-11-26 22:08:42 +0300

    Initial version of Employees generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/employees/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.employees.service.dto;
---
+++ src/test/java/com/employees/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.employees;
+
+import com.employees.EmployeesApp;
+import com.employees.config.AsyncSyncConfiguration;
+import com.employees.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { EmployeesApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
